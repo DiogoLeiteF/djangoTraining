0fd052d9d38025864637558071504f86bf494a0a
Diogo Leite <diogoleitefaria@gmail.com>
2023-06-25 09:16:35 +0100

    feat: add base html and structure

--- password_manager/passmanager/urls.py
+++ password_manager/passmanager/urls.py
@@ -14,14 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, re_path, include
-# from passmanager import views
 
+from django.urls import path, re_path
+from passmanager import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    # path('', views.index, name="Home"),
-    path('', include('passmanager.urls')),
+    
+    path('', views.index, name="Home"),
+    path('manager/', views.manager, name="manager"),
     
 ]
--- password_manager/passmanager/views.py
+++ password_manager/passmanager/views.py
@@ -1,6 +1,28 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from django.http import HttpResponse
+from .forms import LoginForm
+# from django.http import HttpResponse
 
-# Create your views here.
+# Create your views here
+
 def index(request):
-    return HttpResponse("<em>pass manager<em>")
+        # if this is a POST request we need to process the form data
+    if request.method == "POST":
+        # create a form instance and populate it with data from the request:
+        form = LoginForm(request.POST)
+        # check whether it's valid:
+        if form.is_valid():
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            print(form.data)
+            return HttpResponseRedirect("/manager")
+
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = LoginForm()
+
+    return render(request, "passmanager/index.html", {"form": form})
+
+def manager(request):
+    return render(request, 'passmanager/manager.html')
