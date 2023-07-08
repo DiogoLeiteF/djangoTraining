007bd168acebac78695f57a5c2791a5190a53bfc
Diogo Leite <diogoleitefaria@gmail.com>
2023-07-08 04:08:45 +0100

    feat: remove use of forms.py

--- password_manager/passmanager/forms.py
+++ password_manager/passmanager/forms.py
@@ -5,19 +5,3 @@ from django.forms import ModelForm, TextInput, EmailInput
 
     
 
-class LoginForm(forms.Form):
-    # class Meta:
-    #     fields = ['user', 'password']
-    #     widgets = {
-    #         'user': TextInput(attrs={
-    #             'class': "form-control",
-    #             'style': 'max-width: 300px;',
-    #             'placeholder': 'User name'
-    #             }),
-    #         'password': forms.PasswordInput(attrs={
-    #             'class': "form-control", 
-    #             'style': 'max-width: 300px;',
-    #             'placeholder': 'Password'
-    #             })
-    #     }
-    pass
--- password_manager/passmanager/views.py
+++ password_manager/passmanager/views.py
@@ -3,7 +3,6 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
-from .forms import LoginForm
 from django import forms
 
 # from django.http import HttpResponse
@@ -13,19 +12,13 @@ from django import forms
 
 def index(request):
     if request.method == "POST":
-        form = LoginForm(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            username = form.data.get('user')
-            password = form.data.get('password')
-            user = authenticate(username=username, password=password)
-            if user: 
-                login(request, user=user)
-                return HttpResponseRedirect("/manager")
+        username = request.POST.get('user')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+        if user: 
+            login(request, user=user)
+            return HttpResponseRedirect("/manager")
     # if a GET (or any other method) we'll create a blank form
-    else:
-        return render(request, "passmanager/index.html")
-
     return render(request, "passmanager/index.html")
 
 @login_required()
