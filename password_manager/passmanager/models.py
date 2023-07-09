9a69e8fc77f70eaed919f104e853674fd2610518
Diogo Leite <diogoleitefaria@gmail.com>
2023-07-09 06:33:05 +0100

    feat: add created field to list

--- password_manager/passmanager/models.py
+++ password_manager/passmanager/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
 
 class PasswordsList(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    password = models.CharField(max_length=100)
+    password = models.CharField(max_length=100,)
     site = models.URLField()
     notes = models.TextField()
+    created = models.DateField(default=timezone.now())
     def __str__(self) -> str:
         return self.site or self.notes
