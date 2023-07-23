ce6e9689d0876d16cc58d1a9fc11755d256394a8
Diogo Leite <diogoleitefaria@gmail.com>
2023-07-23 06:29:02 +0100

    start DRF REST API tutorial

---
+++ 2023_07_django_REST_API/backend/api/views.py
@@ -0,0 +1,53 @@
+import json
+
+from django.forms.models import model_to_dict
+from django.shortcuts import render
+from django.http import JsonResponse
+
+from products.models import Product
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+
+# Create your views here.
+
+
+# def api_home(request, *args, **kwargs) -> JsonResponse:
+#     body = request.body # byte string of json date
+#     print(request.GET) #access the params form request "/?key=value"
+#     data = {}
+#     try:
+#         data = json.loads(body)
+#     except:
+#         pass
+#     data['headers'] = dict(request.headers)
+#     data['content_type'] = request.content_type
+#     data["params"] = dict(request.GET)
+#     print(data)
+#     return JsonResponse(data)
+
+
+# def api_home(request, *args, **kwargs):
+#     model_data = Product.objects.all().order_by("?").first()
+    
+#     data = {}
+#     if model_data:
+#         # data['id'] = model_data.id
+#         # data['title'] = model_data.title
+#         # data['content'] = model_data.content
+#         # data['price'] = model_data.price
+#         data = model_to_dict(model_data, fields=["id", "price"])
+#     return JsonResponse(data)
+
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    """
+    DRF API View
+    """
+    model_data = Product.objects.all().order_by("?").first()
+    data = {}
+    if model_data:
+        data = model_to_dict(model_data, fields=["id", "price"])
+    return Response(data)
+    
---
+++ 2023_07_django_REST_API/py_client/basic.py
@@ -0,0 +1,14 @@
+import requests
+
+# endpoint = "http://127.0.0.1:8000/"
+endpoint = "http://localhost:8000/api/"
+
+
+get_response = requests.get(endpoint, params={"product_id": 123}, json={"query":"hello world"})
+
+# print(get_response.text)
+# print(get_response.status_code)
+print(get_response.json())
+
+
+
