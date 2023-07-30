7b01f8c45d6e85b0afa103564b05cdf8486785e9
Diogo Leite <diogoleitefaria@gmail.com>
2023-07-30 03:41:03 +0100

    rest generic view retrieve

--- 2023_07_django_REST_API/py_client/detail.py
+++ 2023_07_django_REST_API/py_client/detail.py
@@ -1,11 +1,11 @@
 import requests
 
 # endpoint = "http://127.0.0.1:8000/"
-endpoint = "http://localhost:8000/api/"
+endpoint = "http://localhost:8000/api/products/2"
 
 
 # get_response = requests.get(endpoint, params={"product_id": 123}, json={"query":"hello world"})
-get_response = requests.post(endpoint, json={"query": "hello world"})
+get_response = requests.get(endpoint)
 
 # print(get_response.text)
 # print(get_response.status_code)
