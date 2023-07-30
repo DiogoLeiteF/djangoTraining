35e30ac2f12f881b492c6d9e8c7daa4bd7511ef9
Diogo Leite <diogoleitefaria@gmail.com>
2023-07-30 04:17:07 +0100

    list + list create views

--- 2023_07_django_REST_API/backend/products/urls.py
+++ 2023_07_django_REST_API/backend/products/urls.py
@@ -6,5 +6,5 @@ from . import views
 
 urlpatterns = [
     path("<int:pk>/", view=views.ProductDetailAPIView.as_view()),
-    path("", view=views.ProductCreateAPIView.as_view()),
+    path("", view=views.ProductListCreateAPIView.as_view()),
 ]
--- 2023_07_django_REST_API/py_client/list.py
+++ 2023_07_django_REST_API/py_client/list.py
@@ -3,9 +3,8 @@ import requests
 # endpoint = "http://127.0.0.1:8000/"
 endpoint = "http://localhost:8000/api/products/"
 
-data = {"title": "This is required"}
 
-get_response = requests.post(endpoint, json=data)
+get_response = requests.get(endpoint)
 
 
 print(get_response.json())
